83db2132bb30d2174a0b2debdd7d6e447fed4a68
DaviRodrigues <oversouls11@gmail.com>
2025-09-13 20:44:59 -0300

    test and chore: send email ok, add package aiofiles and remove postgres lib in golang container

--- backend/microservices/src/routers/email.py
+++ backend/microservices/src/routers/email.py
@@ -1,3 +1,4 @@
+import asyncio
 from fastapi import APIRouter, status
 from schemas.Schemas import EmailSchema, send_success, send_err
 from config.Env import Env
@@ -10,7 +11,7 @@ from services.EmailTransport import EmailTransport
 router: APIRouter = APIRouter(prefix="/email", tags=["email"])
 
 
-@router.post("/", status_code=status.HTTP_200_OK)
+@router.post("/")
 async def read_root(email_data: EmailSchema):
     try:
         transport: EmailTransport = TLSTransport(GmailProvider.HOSTNAME, GmailProvider.PORT)
@@ -19,7 +20,8 @@ async def read_root(email_data: EmailSchema):
             return send_err(status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to send email!")
         
         gmail_send: GmailSend = GmailSend(Env.EMAIL_USERNAME, Env.EMAIL_PASSWORD, email_data.subject, transport)
-        await gmail_send.send_email(EmailSchema.send_to, EmailSchema.template_name, EmailSchema.data)
+        asyncio.create_task(
+            gmail_send.send_email(email_data.send_to, email_data.template_name, email_data.data))
         return send_success("Email send success!")
     except Exception as e:
         print(f"Failed to send email, error: {e}")
