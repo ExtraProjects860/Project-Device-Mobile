06463eeeaef0a4116d6dcb11b713320f57a641b1
DaviRodrigues <oversouls11@gmail.com>
2025-09-18 23:52:26 -0300

    raw: move model several files to only file, to more legibility code

---
+++ backend/api/model/model.go
@@ -0,0 +1,70 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	TypeUserID     uint   `gorm:"not null"`
+	Name           string `gorm:"not null"`
+	Email          string `gorm:"uniqueIndex;not null"`
+	Password       string `gorm:"not null"`
+	Cpf            string `gorm:"uniqueIndex;not null"`
+	RegisterNumber uint   `gorm:"not null"`
+	PhotoUrl       *string
+
+	TypeUser      TypeUser      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	TokenPassword TokenPassword `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	WishLists     []WishList    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+type TokenPassword struct {
+	gorm.Model
+	UserID uint `gorm:"unique;not null"`
+	Code   *string
+	TimeUp *time.Time
+	User   *User
+}
+
+type TypeUser struct {
+	gorm.Model
+	Name  string `gorm:"unique;not null"`
+	Users []User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+// dps vou ter que usar aqui um unique de junção
+type WishList struct {
+	gorm.Model
+	UserID    uint `gorm:"uniqueIndex:idx_wish_list_group;not null"`
+	ProductID uint `gorm:"uniqueIndex:idx_wish_list_group;not null"`
+
+	User    User    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Product Product `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+func (WishList) TableName() string {
+	return "wish_lists"
+}
+
+type Product struct {
+	gorm.Model
+	Name        string  `gorm:"uniqueIndex;not null"`
+	Description string  `gorm:"not null"`
+	Value       float64 `gorm:"type:decimal(10,2);not null"`
+	Quantity    int     `gorm:"not null"`
+	PhotoUrl    *string
+	IsAvaible   bool `gorm:"default:true;not null"`
+
+	Promotions []Promotion `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+type Promotion struct {
+	gorm.Model
+	ProductID uint    `gorm:"uniqueIndex;not null"`
+	Product   Product `gorm:"not null"`
+	Discount  *float64
+	IsAvaible bool `gorm:"default:true;not null"`
+}
