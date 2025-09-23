872c789498f070f0e27a51bb8c32e455ee86955b
DaviRodrigues <oversouls11@gmail.com>
2025-09-23 01:50:29 -0300

    feat: configure axios instance with base URL and default settings

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,2 +1,9 @@
 import axios from "axios";
 
+const instance = axios.create({
+  baseURL: "http://localhost:5050",
+});
+
+instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.timeout = 15
+instance.defaults.withCredentials = true;
