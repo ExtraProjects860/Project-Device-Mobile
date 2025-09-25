afd3d0a8530d79c3db6fe877102366d29139f082
DaviRodrigues <oversouls11@gmail.com>
2025-09-25 00:48:26 -0300

    feat: adding SearchBar implementation and alocated in UserScreen to tests

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useThemeColors } from "../hooks/useThemeColors.js";
+
+export default function SearchBar() {
+  const [search, setSearch] = useState();
+
+  const themeColors = useThemeColors();
+
+  return (
+    <View className="flex-row w-full px-5 items-center gap-x-2">
+      <View className="flex-1 flex-row items-center bg-white rounded-full px-4 py-2 gap-x-3">
+        <TouchableOpacity>
+          <Icon name="menu" size={24} />
+        </TouchableOpacity>
+        <TextInput
+          placeholder="Buscar"
+          returnKeyType="search"
+          value={search}
+          onChangeText={setSearch}
+          className="flex-1 text-base text-light-text-primary dark:text-dark-text-primary"
+        />
+        <TouchableOpacity className="px-2">
+          <Icon name="magnify" size={24} />
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <TouchableOpacity className="flex-row items-center bg-light-secundary dark:bg-dark-sencondary rounded-full p-4 shadow-md">
+          <Icon
+            name="account-plus-outline"
+            size={24}
+            color={themeColors.header}
+          />
+          <Text className="ml-2 text-white font-semibold">Add</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        
+      </View>
+    </View>
+  );
+}
--- frontend/src/screens/UsersScreen.jsx
+++ frontend/src/screens/UsersScreen.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { useCallback } from "react";
 import { View, Text } from "react-native";
 import Background from "../components/ui/Background";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { NavBar } from "../components/Navbar";
+import { useThemeColors } from "../hooks/useThemeColors.js";
+import SearchBar from "../components/SearchBar.jsx";
 import PageLoader from "../context/PageLoader.js";
 
 export default function UsersScreen() {
+  const themeColors = useThemeColors();
+
   const fetchUsers = useCallback(async (setData) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     setData({});
@@ -16,10 +21,16 @@ export default function UsersScreen() {
       {(data) => (
         <Background>
           <NavBar />
-          <View className="flex-1 items-center justify-center">
-            <Text className="text-xl color-white font-bold text-slate-800">
-              Esta é a Tela de Usuários
-            </Text>
+          <View className="flex-row gap-2 m-6 items-center justify-center">
+            <Icon
+              name="account-group-outline"
+              size={30}
+              color={themeColors.header}
+            />
+            <Text className="text-white font-bold text-3xl">Usuários</Text>
+          </View>
+          <View className="items-center ">
+            <SearchBar />
           </View>
         </Background>
       )}
