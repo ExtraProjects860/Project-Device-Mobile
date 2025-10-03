891a4201de6fbc99d50841bca4bb968ddde03342
DaviRodrigues <oversouls11@gmail.com>
2025-10-03 01:54:24 -0300

    refactor: implemented escope list to render items for user, adding too comentaries to after implementation

--- frontend/src/screens/WishListScreen.jsx
+++ frontend/src/screens/WishListScreen.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import { View, Text } from "react-native";
 import Background from "../components/ui/Background";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { NavBar } from "../components/Navbar";
+import SearchBar from "../components/SearchBar.jsx";
+import ListItems from "../components/ListItems.jsx";
+import ButtonAdd from "../components/ui/ButtonAdd.jsx";
+import { useThemeColors } from "../hooks/useThemeColors.js";
 
 export default function ProductsScreen() {
+  // TODO essa lista aqui, pode ser implementada só pra teste de renderização
+  // A questão toda que será necessário implementar o login para tokens
+  // Manter o usuário na tela atual
+  // Fazer com que ele não seja deslogado e verifique se o token dele está ok
+  // botão de lembrar de mim
+  // logout para limpar
+  // adicionar secureStore
+  // Implementar tela offline para user
+  const themeColors = useThemeColors();
+
   return (
     <Background>
       <NavBar />
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-xl color-white font-bold text-slate-800">
-          Esta é a Tela de Lista de Desejos
-        </Text>
+
+      <View className="flex-row gap-2 m-6 items-center justify-center">
+        <Icon
+          name="account-group-outline"
+          size={30}
+          color={themeColors.header}
+        />
+        <Text className="text-white font-bold text-3xl">Usuários</Text>
+      </View>
+
+      <View className="items-center mb-2">
+        <SearchBar buttonAdd={<ButtonAdd />} />
       </View>
     </Background>
   );
