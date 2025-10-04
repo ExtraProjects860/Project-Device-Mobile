4feb443245c1350a7de15d376923e6d6a87939ad
DaviRodrigues <oversouls11@gmail.com>
2025-10-04 17:17:26 -0300

    feat: adding Data Access Object to more abstraction and flexibilty in project

---
+++ backend/api/repository/dao.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
+// TODO ajustar retornos para colocar como DTO
+// Adicionar delete na wishlist
+
+func getByID[T any](ctx context.Context, query *gorm.DB, id uint) (*T, error) {
+	var model T
+	err := query.First(&model, id).Error
+	return &model, err
+}
+
+func create[T any](ctx context.Context, db *gorm.DB, entity *T) error {
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		return tx.Create(entity).Error
+	})
+}
+
+func update[T any](ctx context.Context, db *gorm.DB, id uint, entity *T) error {
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		return tx.Model(entity).Where("id = ?", id).Updates(entity).Error
+	})
+}
+
+func deleteByID[T any](ctx context.Context, db *gorm.DB, id uint) error {
+	var entity T
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		return tx.Where("id = ?", id).Delete(&entity).Error
+	})
+}
+
+func getByPagination[T any](query *gorm.DB, itemsPerPage, currentPage uint) ([]T, uint, uint, error) {
+	var models []T
+
+	offset, totalPages, totalItems := pagination(query, itemsPerPage, currentPage)
+
+	err := query.
+		Limit(int(itemsPerPage)).
+		Offset(int(offset)).
+		Find(&models).Error
+
+	return models, totalPages, totalItems, err
+}
