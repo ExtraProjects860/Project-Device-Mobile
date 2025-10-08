d7b168b4b2046c472225a5e2ea1f1dd4f7fe6b20
DaviRodrigues <oversouls11@gmail.com>
2025-10-08 13:22:41 -0300

    cleanup: remove TODO commentary about move interface ValidateModel to Request module

--- backend/api/schemas/schemas.go
+++ backend/api/schemas/schemas.go
@@ -1,11 +1,5 @@
 package schemas
 
-/*
-TODO
-Remover validate e format de outros schemas e passar para request como struct seguindo modelo
-do userRequest, além disso, passar a interface do validate para o request e anexar ela para uso
-*/
-
 func AllModelsSlice() []any {
 	return []any{
 		&User{},
