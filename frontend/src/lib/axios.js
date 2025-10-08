10ba1ddea82be099a53004c5cb2860b0d0e65cd6
DaviRodrigues <oversouls11@gmail.com>
2025-10-08 10:31:23 -0300

    docs: adding warning commentary in instanceMain axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// TODO não esquecer de mudar sempre a URL do axios quando for utilizar
 export const instanceMainApi = axios.create({
   baseURL: "https://thermogenous-jimmie-manly.ngrok-free.dev/api/v1",
 });
