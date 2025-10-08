185462e133179b20b5db9b950fcdbb751857ae79
DaviRodrigues <oversouls11@gmail.com>
2025-10-08 10:14:05 -0300

    refactor: removed prepare function

--- backend/api/utils/validators.go
+++ backend/api/utils/validators.go
@@ -1,9 +1,6 @@
 package utils
 
 import (
-	"context"
-
-	"github.com/ExtraProjects860/Project-Device-Mobile/schemas"
 	"github.com/go-playground/validator/v10"
 	"github.com/paemuri/brdoc"
 )
@@ -11,11 +8,6 @@ import (
 func validateCPF(fl validator.FieldLevel) bool {
 	cpf := fl.Field().String()
 	return brdoc.IsCPF(cpf)
-}
-
-func PrepareAndValidate(ctx context.Context, schema schemas.ValidateModel, validate *validator.Validate) error {
-	schema.Format()
-	return schema.Validate(ctx, validate)
 }
 
 func GetValidate() *validator.Validate {
