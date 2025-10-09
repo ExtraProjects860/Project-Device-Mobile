284c64dd28bd83656e581cd6a166c4f7e4bfdc08
DaviRodrigues <oversouls11@gmail.com>
2025-10-08 23:38:04 -0300

    feat: implemented update user, and validateAndUpdateFields

--- backend/api/service/userService.go
+++ backend/api/service/userService.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ExtraProjects860/Project-Device-Mobile/handler/request"
 	"github.com/ExtraProjects860/Project-Device-Mobile/repository"
@@ -9,6 +10,7 @@ import (
 	"github.com/ExtraProjects860/Project-Device-Mobile/service/dto"
 	"github.com/ExtraProjects860/Project-Device-Mobile/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/paemuri/brdoc"
 )
 
 type UserService struct {
@@ -17,6 +19,39 @@ type UserService struct {
 
 func NewUserService(repo repository.PostgresUserRepository) UserService {
 	return UserService{repo: repo}
+}
+
+func validateAndUpdateFields(user *schemas.User, input request.UserRequest) error {
+	if input.Name != "" {
+		user.Name = input.Name
+	}
+	if input.Email != "" {
+		user.Email = input.Email
+	}
+	if input.Password != "" {
+		hashed, err := utils.GenerateHashPassword(input.Password)
+		if err != nil {
+			return fmt.Errorf("password hash: %v", err)
+		}
+		user.Password = hashed
+	}
+	if input.Cpf != "" && brdoc.IsCPF(input.Cpf) {
+		user.Cpf = input.Cpf
+	}
+	if input.RegisterNumber != 0 {
+		user.RegisterNumber = input.RegisterNumber
+	}
+	if input.RoleID != 0 {
+		user.RoleID = input.RoleID
+	}
+	if input.EnterpriseID != nil && *input.EnterpriseID != 0 {
+
+		user.EnterpriseID = input.EnterpriseID
+	}
+	if input.PhotoUrl != nil && *input.PhotoUrl != "" {
+		user.PhotoUrl = input.PhotoUrl
+	}
+	return nil
 }
 
 func (u *UserService) Create(ctx context.Context, input request.UserRequest) (*dto.UserDTO, error) {
@@ -30,23 +65,34 @@ func (u *UserService) Create(ctx context.Context, input request.UserRequest) (*d
 		EnterpriseID:   input.EnterpriseID,
 		Name:           input.Name,
 		Email:          input.Email,
-		Password:       hashedPassword, 
+		Password:       hashedPassword,
 		Cpf:            input.Cpf,
 		RegisterNumber: input.RegisterNumber,
 		PhotoUrl:       input.PhotoUrl,
 	}
 
-	err = u.repo.CreateUser(ctx, &user)
-	if err != nil {
+	if err = u.repo.CreateUser(ctx, &user); err != nil {
 		return nil, err
 	}
 
 	return dto.MakeUserOutput(user), nil
 }
 
+func (u *UserService) Update(ctx *gin.Context, id uint, input request.UserRequest) (*dto.UserDTO, error) {
+	user, err := u.repo.GetInfoUser(ctx, id)
+	if err != nil {
+		return nil, err
+	}
 
-func (u *UserService) Update(ctx *gin.Context, id uint, user schemas.User) {
+	if err = validateAndUpdateFields(&user, input); err != nil {
+		return nil, err
+	}
 
+	if err = u.repo.UpdateUser(ctx, id, &user); err != nil {
+		return nil, err
+	}
+
+	return dto.MakeUserOutput(user), nil
 }
 
 func (u *UserService) Get(ctx context.Context, id uint) (*dto.UserDTO, error) {
