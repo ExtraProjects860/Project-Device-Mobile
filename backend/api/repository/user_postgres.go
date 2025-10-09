3a5c005531a80c3c022a8608133cc444af629173
DaviRodrigues <oversouls11@gmail.com>
2025-10-08 23:41:20 -0300

    feat: implemented verifyDuplicated in repository user and update method

--- backend/api/repository/user_postgres.go
+++ backend/api/repository/user_postgres.go
@@ -7,7 +7,22 @@ import (
 
 	"github.com/ExtraProjects860/Project-Device-Mobile/schemas"
 	"github.com/jackc/pgx/v5/pgconn"
+	"gorm.io/gorm"
 )
+
+func verifyDuplicated(err error) error {
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+		if strings.Contains(pgErr.ConstraintName, "email") {
+			return errors.New("this email is registered")
+		}
+		if strings.Contains(pgErr.ConstraintName, "cpf") {
+			return errors.New("this cpf is registered")
+		}
+		return err
+	}
+	return err
+}
 
 func (r *PostgresUserRepository) GetInfoUser(ctx context.Context, id uint) (schemas.User, error) {
 	query := r.db.WithContext(ctx).Model(&schemas.User{}).Preload("Role").Preload("Enterprise")
@@ -22,27 +37,38 @@ func (r *PostgresUserRepository) GetInfoUser(ctx context.Context, id uint) (sche
 func (r *PostgresUserRepository) CreateUser(ctx context.Context, user *schemas.User) error {
 	err := create(ctx, r.db, user)
 	if err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
-			if strings.Contains(pgErr.ConstraintName, "email") {
-				return errors.New("this email is registered")
-			}
-			if strings.Contains(pgErr.ConstraintName, "cpf") {
-				return errors.New("this cpf is registered")
-			}
-			return err
-		}
-		return err
+		return verifyDuplicated(err)
 	}
 
-	return r.db.WithContext(ctx).
-		Preload("Role").
-		Preload("Enterprise").
-		First(user, user.ID).Error
+	u, err := r.GetInfoUser(ctx, user.ID)
+	if err != nil {
+		return err
+	}
+	*user = u
+
+	return nil
 }
 
-func (r *PostgresUserRepository) UpdateUser(context.Context, uint, *schemas.User) error {
-	panic("Not implemented")
+func (r *PostgresUserRepository) UpdateUser(ctx context.Context, id uint, user *schemas.User) error {
+	err := r.db.
+		WithContext(ctx).
+		Model(&schemas.User{}).
+		Transaction(func(tx *gorm.DB) error {
+			return tx.
+				Where("id = ?", id).
+				Updates(&user).Error
+		})
+	if err != nil {
+		return verifyDuplicated(err)
+	}
+
+	u, err := r.GetInfoUser(ctx, user.ID)
+	if err != nil {
+		return err
+	}
+	*user = u
+
+	return nil
 }
 
 func (r *PostgresUserRepository) GetUsers(ctx context.Context, itemsPerPage uint, currentPage uint) ([]schemas.User, uint, uint, error) {
