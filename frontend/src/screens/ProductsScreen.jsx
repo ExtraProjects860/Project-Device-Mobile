2def8fb52c7b07f2bbecb4ca374f34aadfd3f596
PauloOtavioDuarteRocha <paulotavioduarterocha@hotmail.com>
2025-10-10 21:07:18 -0300

    feat: Create modal Add Product

--- frontend/src/screens/ProductsScreen.jsx
+++ frontend/src/screens/ProductsScreen.jsx
@@ -9,22 +9,36 @@ import ButtonAdd from "../components/ui/ButtonAdd.jsx";
 import CardProductList from "../components/ui/CardProductList.jsx";
 import { useThemeColors } from "../hooks/useThemeColors.js";
 import { getProductsRequest } from "../lib/ProductsRequest.js";
+import { useState } from "react";
+import ModalCreate from "../components/ModalCreateProduct.jsx";
 
 export default function ProductsScreen() {
   const themeColors = useThemeColors();
+  const [isCreateProductModalVisible, setCreateProductVisible] = useState(false);
+
 
   return (
+    
+
     <Background>
+      <ModalCreate
+        visible={isCreateProductModalVisible}
+        onClose={() => setCreateProductVisible(false)}
+  
+      />
       <NavBar />
 
       <View className="flex-row gap-2 m-6 items-center justify-center">
         <Icon name="shopping-outline" size={30} color={themeColors.header} />
         <Text className="text-white font-bold text-3xl">Produtos</Text>
       </View>
+      
+
 
       <View className="items-center mb-2">
-        <SearchBar buttonAdd={<ButtonAdd />} />
+        <SearchBar buttonAdd={<ButtonAdd onPress={() => setCreateProductVisible(true)}/>} />
       </View>
+
 
       <ListItems
         callbackFetch={getProductsRequest}
