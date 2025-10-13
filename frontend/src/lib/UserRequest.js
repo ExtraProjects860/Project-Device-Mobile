7b4b5cfef73dffbcba5bb65f7619ba99ea1e0d95
Evandro Machado <evandromachado2006@gmail.com>
2025-10-13 10:11:34 -0300

    feat: Add parameter support to create and update product/user requests

--- frontend/src/lib/UserRequest.js
+++ frontend/src/lib/UserRequest.js
@@ -8,14 +8,27 @@ export async function getUsersRequest(itemsPerPage = 20, currentPage = 1) {
   return response.data || [];
 }
 
-export async function createUserRequest() {
-  return;
+export async function createUserRequest(userData) {
+  const response = await instanceMainApi.post("/users", userData);
+  return response.data;
 }
 
-export async function getInfoUserRequest() {
-  return;
+export async function getInfoUserRequest(userId) {
+  const response = await instanceMainApi.get(`/users/${userId}`);
+  return response.data;
 }
 
-export async function updateUserRequest() {
-  return;
+/**
+ * @param {string} userId
+ * @param {object} updatedUserData
+ * @param {string} [updatedUserData.name]
+ * @param {string} [updatedUserData.register_number]
+ * @param {string} [updatedUserData.email]
+ */
+export async function updateUserRequest(userId, updatedUserData) {
+  const response = await instanceMainai.patch(
+    `/users/${userId}`,
+    updatedUserData,
+  );
+  return response.data;
 }
