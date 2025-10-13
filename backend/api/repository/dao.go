efd6b73f5b08bfd2d577fa866653a01adc311bb5
DaviRodrigues <oversouls11@gmail.com>
2025-10-13 10:34:39 -0300

    feat: add generic base for database operations. Creates a suite of generic functions to handle common GORM database interactions. This provides a standardized and reusable way to perform Create, Read, Update, and Delete (CRUD) operations, as well as custom queries and pagination, eliminating redundant code across different entity repositories.

--- backend/api/repository/dao.go
+++ backend/api/repository/dao.go
@@ -6,33 +6,54 @@ import (
 	"gorm.io/gorm"
 )
 
-// TODO Adicionar delete na wishlist
-
-func getByID[T any](query *gorm.DB, id uint) (T, error) {
+func firstWhere[T any](db *gorm.DB, query string, args ...any) (T, error) {
 	var model T
-	err := query.First(&model, id).Error
+	err := db.Where(query, args...).First(&model).Error
+	return model, err
+}
+
+func getByID[T any](db *gorm.DB, id uint) (T, error) {
+	var model T
+	err := db.First(&model, id).Error
 	return model, err
 }
 
 func create[T any](ctx context.Context, db *gorm.DB, entity *T) error {
 	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
-		return tx.Create(entity).Error
+		return tx.Model(new(T)).Create(entity).Error
 	})
 }
 
-func deleteByID[T any](ctx context.Context, db *gorm.DB, id uint) error {
-	var entity T
+func updateByID[T any](ctx context.Context, db *gorm.DB, entity *T, id uint) error {
 	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
-		return tx.Where("id = ?", id).Delete(&entity).Error
+		return tx.Model(new(T)).Where("id = ?", id).Updates(entity).Error
 	})
 }
 
-func getByPagination[T any](query *gorm.DB, itemsPerPage, currentPage uint) ([]T, uint, uint, error) {
+func update[T any](ctx context.Context, db *gorm.DB, entity *T, queryWhere string, args ...any) error {
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		return tx.Model(new(T)).Where(queryWhere, args...).Updates(entity).Error
+	})
+}
+
+func deleteByID[T any](ctx context.Context, db *gorm.DB, entity *T, id uint) error {
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		return tx.Model(new(T)).Where("id = ?", id).Delete(entity).Error
+	})
+}
+
+func delete[T any](ctx context.Context, db *gorm.DB, entity *T, queryWhere string, args ...any) error {
+	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		return tx.Model(new(T)).Where(queryWhere, args...).Delete(entity).Error
+	})
+}
+
+func getByPagination[T any](db *gorm.DB, itemsPerPage, currentPage uint) ([]T, uint, uint, error) {
 	var models []T
 
-	offset, totalPages, totalItems := pagination(query, itemsPerPage, currentPage)
+	offset, totalPages, totalItems := pagination(db, itemsPerPage, currentPage)
 
-	err := query.
+	err := db.
 		Limit(int(itemsPerPage)).
 		Offset(int(offset)).
 		Find(&models).Error
