c73e848af500587f30742b61bec39e9dde384bab
DaviRodrigues <oversouls11@gmail.com>
2025-10-14 22:53:37 -0300

    feat(migrate): refactor migration functions to include logger for improved error handling and logging

--- backend/api/db/migrate/main.go
+++ backend/api/db/migrate/main.go
@@ -6,14 +6,15 @@ import (
 	"github.com/ExtraProjects860/Project-Device-Mobile/config"
 )
 
-var logger *config.Logger = config.GetLogger("migrate")
-
 func main() {
-	if err := config.Init(); err != nil {
+	logger := config.NewLogger("migrate")
+
+	_, db, err := config.Init()
+	if err != nil {
 		panic(fmt.Errorf("failed to init config: %v", err))
 	}
 
-	if err := migrateDatabase(config.GetDB()); err != nil {
+	if err := migrateDatabase(db, logger); err != nil {
 		logger.Errorf("AutoMigrate error: %v", err)
 		panic(fmt.Errorf("failed migrate models to database: %v", err))
 	}
