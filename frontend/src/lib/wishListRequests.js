bcb2a5ca1243b4ecdfb93d71ad5f53ebd2ea316b
DaviRodrigues <oversouls11@gmail.com>
2025-10-24 01:47:02 -0300

    feat: implemented getItemsWishList request

--- frontend/src/lib/wishListRequests.js
+++ frontend/src/lib/wishListRequests.js
@@ -8,6 +8,24 @@ export async function deleteWishListRequest() {
   return;
 }
 
-export async function getItemsWishListRequest() {
-  return;
+/**
+ * @param {number} itemsPerPage
+ * @param {number} currentPage
+ * @param {string} accessToken
+ */
+export async function getItemsWishListRequest(
+  itemsPerPage = 20,
+  currentPage = 1,
+  accessToken,
+) {
+  const response = await instanceMainApi.get(
+    `/wishlist?itemsPerPage=${itemsPerPage}&currentPage=${currentPage}`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+
+  return response.data || [];
 }
