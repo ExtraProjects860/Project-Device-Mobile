abb03635c34ac619451b7849435478bf82090e44
DaviRodrigues <oversouls11@gmail.com>
2025-10-24 01:51:13 -0300

    feat: implemented render WishListItems, but not adding modal remove item

--- frontend/src/screens/WishListScreen.jsx
+++ frontend/src/screens/WishListScreen.jsx
@@ -1,39 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import Background from "../components/ui/Background";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { NavBar } from "../components/Navbar";
 import SearchBar from "../components/SearchBar";
-import ButtonAdd from "../components/ui/ButtonAdd";
+import CardWishListItem from "../components/ui/CardWishListItem";
+import ListItems from "../components/ListItems";
 import { useThemeColors } from "../hooks/useThemeColors.js";
+import { useHandleRefresh } from "../hooks/useHandleRefresh.js";
+import { getItemsWishListRequest } from "../lib/wishListRequests.js";
 
 export default function ProductsScreen() {
-  // TODO essa lista aqui, pode ser implementada só pra teste de renderização
-  // A questão toda que será necessário implementar o login para tokens
-  // Manter o usuário na tela atual
-  // Fazer com que ele não seja deslogado e verifique se o token dele está ok
-  // botão de lembrar de mim
-  // logout para limpar
-  // adicionar secureStore
-  // Implementar tela offline para user
+  // TODO essa lista aqui, falta o modal de deletar items
   const themeColors = useThemeColors();
+  const { listKey, handleRefresh } = useHandleRefresh();
+
+  const [selectedWishListItem, setSelectedWishListItem] = useState(null);
+
+  const handleRemoveProduct = () => {};
+
+  const CardWishListItemRender = ({ item }) => (
+    <CardWishListItem item={item} onRemove={handleRemoveProduct} />
+  );
 
   return (
     <Background>
       <NavBar />
 
       <View className="flex-row gap-2 m-6 items-center justify-center">
-        <Icon
-          name="account-group-outline"
-          size={30}
-          color={themeColors.header}
-        />
-        <Text className="text-white font-bold text-3xl">Usuários</Text>
+        <Icon name="bookmark-outline" size={30} color={themeColors.header} />
+        <Text className="text-white font-bold text-3xl">Lista de Desejos</Text>
       </View>
 
       <View className="items-center mb-2">
-        <SearchBar buttonAdd={<ButtonAdd />} />
+        <SearchBar />
       </View>
+
+      <ListItems
+        ref={listKey}
+        callbackFetch={getItemsWishListRequest}
+        CardListRender={CardWishListItemRender}
+      />
     </Background>
   );
 }
