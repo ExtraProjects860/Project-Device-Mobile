44d6b35ce0b433426dd5ac630fa3a66e27890eca
DaviRodrigues <oversouls11@gmail.com>
2025-10-24 01:50:19 -0300

    refactor: fix form login style

--- frontend/src/screens/autentication/LoginScreen.jsx
+++ frontend/src/screens/autentication/LoginScreen.jsx
@@ -16,9 +16,12 @@ import { loginRequest } from "../../lib/authRequests.js";
 import { getInfoUserRequest } from "../../lib/userRequests.js";
 import { useNavigateTo } from "../../hooks/useNavigateTo.js";
 import { useAppContext } from "../../context/AppContext.js";
+import { useThemeColors } from "../../hooks/useThemeColors.js";
 
 export default function LoginScreen() {
   const { updateToken, updateUser } = useAppContext();
+  const themeColors = useThemeColors();
+
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -41,11 +44,11 @@ export default function LoginScreen() {
     const token = await loginRequest(userData);
 
     if (!token) {
-      throw new Error("Não foi possível recuperar o token de acesso")
-    } 
+      throw new Error("Não foi possível recuperar o token de acesso");
+    }
 
     await updateToken(token);
-    
+
     return token;
   };
 
@@ -53,7 +56,7 @@ export default function LoginScreen() {
     const userData = await getInfoUserRequest(accessToken);
 
     if (!userData) {
-      throw new Error("Não foi possível recuperar os dados do usuário")
+      throw new Error("Não foi possível recuperar os dados do usuário");
     }
 
     await updateUser(userData);
@@ -114,10 +117,8 @@ export default function LoginScreen() {
         className="bg-light-primary dark:bg-dark-primary"
       />
       <View className="flex-1 justfy-center items-center align-middle mt-16">
-        {/* Logo */}
         <Logo className="size-48 mb-4" resizedMode="center" />
 
-        {/* Titulo */}
         <View>
           <Text className="text-light-text-inverted font-bold text-4xl mb-4">
             Olá! Seja Bem Vindo!
@@ -125,61 +126,69 @@ export default function LoginScreen() {
         </View>
         <View className="w-96 h-0.5 bg-light-card mb-16" />
 
-        {/* Campo E-mail */}
-        <View className="mb-6">
-          <View className="flex-row items-center mb-2">
-            <MaterialIcons name="email" size={24} color="white" />
-            <Text className="text-light-text-inverted font-bold pl-2 text-2xl">
-              E-mail:
-            </Text>
-          </View>
-          <View className="flex-row items-center bg-white rounded-full px-4 py-1 w-5/6">
-            <TextInput
-              className="-py-1 ml-2 flex-1 text-light-text-secondary font-semibold text-2xl"
-              placeholder="Exemplo@gmail.com"
-              placeholderTextColor="gray"
-              keyboardType="email-address"
-              value={email}
-              onChangeText={setEmail}
-            />
-          </View>
-        </View>
+        <View className="w-96">
+          <View className="mb-6">
+            <View className="flex-row items-center mb-2">
+              <MaterialIcons name="email" size={24} color="white" />
+              <Text className="text-light-text-inverted font-bold pl-2 text-2xl">
+                E-mail:
+              </Text>
+            </View>
 
-        {/* Campo Senha */}
-        <View className="mb-6">
-          <View className="flex-row items-center mb-2">
-            <MaterialIcons name="lock" size={24} color="white" />
-            <Text className="text-light-text-inverted font-bold pl-2 text-2xl">
-              Senha:
-            </Text>
-          </View>
-          <View className="flex-row items-center bg-white rounded-full px-4 py-1 w-5/6">
-            <TextInput
-              className="-py-1 ml-2 flex-1 text-light-text-secondary font-semibold text-2xl"
-              secureTextEntry={!showPassword}
-              placeholder="*********"
-              placeholderTextColor="gray"
-              value={password}
-              onChangeText={setPassword}
-            />
-            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-              <Feather
-                name={showPassword ? "eye-off" : "eye"}
-                size={20}
-                color="#475569"
+            <View className="flex-row items-center bg-white rounded-full px-4 py-1 w-full">
+              <TextInput
+                className="-py-1 ml-2 flex-1 text-light-text-secondary font-semibold text-2xl"
+                placeholder="Exemplo@gmail.com"
+                placeholderTextColor="gray"
+                keyboardType="email-address"
+                value={email}
+                onChangeText={setEmail}
               />
-            </TouchableOpacity>
+            </View>
+          </View>
+
+          <View className="mb-6">
+            <View className="flex-row items-center mb-2">
+              <MaterialIcons name="lock" size={24} color="white" />
+              <Text className="text-light-text-inverted font-bold pl-2 text-2xl">
+                Senha:
+              </Text>
+            </View>
+
+            <View className="flex-row items-center bg-white rounded-full px-4 py-1 w-full">
+              <TextInput
+                className="-py-1 ml-2 flex-1 text-light-text-secondary font-semibold text-2xl"
+                secureTextEntry={!showPassword}
+                placeholder="*********"
+                placeholderTextColor="gray"
+                value={password}
+                onChangeText={setPassword}
+              />
+
+              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <Feather
+                  name={showPassword ? "eye-off" : "eye"}
+                  size={20}
+                  color="#475569"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <View className="flex-row items-center">
+            <Switch
+              value={rememberMe}
+              trackColor={themeColors.switch.track}
+              thumbColor={themeColors.switch.thumb}
+              onValueChange={setRememberMe}
+            />
+
+            <Text className="text-light-text-inverted font-bold">
+              Lembrar de Mim
+            </Text>
           </View>
         </View>
 
-        <View className="mb-6 flex-row items-center">
-          <Switch value={rememberMe} onValueChange={setRememberMe} />
-          <Text className="text-light-text-inverted font-bold">
-            Lembrar de Mim
-          </Text>
-        </View>
-
-        {/* Botões */}
         <View className="w-full items-center flex-1">
           <TouchableOpacity
             onPress={handleLogin}
