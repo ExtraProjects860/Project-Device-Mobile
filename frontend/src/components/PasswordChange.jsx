e8dceb0da2b701e384f82d8237800903e7480d18
DaviRodrigues <oversouls11@gmail.com>
2025-10-25 19:31:46 -0300

    refactor: apply theme changes in passwordChange modal

--- frontend/src/components/PasswordChange.jsx
+++ frontend/src/components/PasswordChange.jsx
@@ -8,10 +8,13 @@ import {
   Pressable,
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useThemeColors } from "../hooks/useThemeColors.js";
 
 export default function PasswordChangeModal({ visible, onClose }) {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const themeColors = useThemeColors();
 
   const handlePasswordChange = () => {
     console.log("Senha alterada!");
@@ -25,26 +28,38 @@ export default function PasswordChangeModal({ visible, onClose }) {
       onRequestClose={onClose}
     >
       <View className="flex-1 justify-center items-center bg-black/40 px-5">
-        <View className="bg-light-card w-full p-6 rounded-2xl">
+        <View className="bg-light-card dark:bg-dark-card w-full p-6 rounded-2xl">
           {/* Header */}
           <View className="flex-row justify-between items-center mb-6">
             <View className="w-8" />
 
-            <Text className="text-xl font-bold text-light-text-primary">
+            <Text className="text-xl font-bold text-light-text-primary dark:text-dark-text-primary">
               Troca de Senha
             </Text>
 
             <Pressable onPress={onClose} className="p-1">
-              <Icon name="close" size={24} />
+              <Icon
+                color={
+                  themeColors.primary === "#FFFFFF" ? "#A0A0A0" : "#6B7280"
+                }
+                name="close"
+                size={24}
+              />
             </Pressable>
           </View>
 
           {/* Nova Senha */}
           <View className="mb-4">
             <View className="flex-row items-center mb-2">
-              <Icon name="lock-outline" size={24} color="gray" />
+              <Icon
+                color={
+                  themeColors.primary === "#FFFFFF" ? "#A0A0A0" : "#6B7280"
+                }
+                name="lock-outline"
+                size={24}
+              />
 
-              <Text className="ml-2 text-gray-strong text-xl font-semibold">
+              <Text className="ml-2 text-light-text-primary dark:text-dark-text-primary text-xl font-semibold">
                 Nova Senha:
               </Text>
             </View>
@@ -53,6 +68,9 @@ export default function PasswordChangeModal({ visible, onClose }) {
               className="bg-gray-soft rounded-full p-4 text-base"
               placeholder="********"
               secureTextEntry
+              placeholderTextColor={
+                themeColors.primary === "#FFFFFF" ? "#A0A0A0" : "#6B7280"
+              }
               value={newPassword}
               onChangeText={setNewPassword}
             />
@@ -63,7 +81,7 @@ export default function PasswordChangeModal({ visible, onClose }) {
             <View className="flex-row items-center mb-2">
               <Icon name="lock-outline" size={24} color="gray" />
 
-              <Text className="ml-2 text-gray-strong text-xl font-semibold">
+              <Text className="ml-2 text-light-text-primary dark:text-dark-text-primary text-xl font-semibold">
                 Confirmar Senha:
               </Text>
             </View>
@@ -72,6 +90,9 @@ export default function PasswordChangeModal({ visible, onClose }) {
               className="bg-gray-soft rounded-full p-4 text-base"
               placeholder="********"
               secureTextEntry
+              placeholderTextColor={
+                themeColors.primary === "#FFFFFF" ? "#A0A0A0" : "#6B7280"
+              }
               value={confirmPassword}
               onChangeText={setConfirmPassword}
             />
@@ -80,7 +101,7 @@ export default function PasswordChangeModal({ visible, onClose }) {
           {/* Salvar */}
           <TouchableOpacity
             onPress={handlePasswordChange}
-            className="bg-light-primary p-3 rounded-full flex-row justify-center items-center"
+            className="bg-light-primary dark:bg-dark-secondary p-3 rounded-full flex-row justify-center items-center"
           >
             <Icon name="check-bold" size={20} color="white" />
 
