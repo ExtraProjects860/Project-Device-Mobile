b826cf1474ddb258283963e4dafea2c61355b4c5
DaviRodrigues <oversouls11@gmail.com>
2025-10-25 02:56:03 -0300

    refactor: changing method get data to pagination to pagination module in requests

--- backend/api/handler/request/requests.go
+++ backend/api/handler/request/requests.go
@@ -44,30 +44,6 @@ func GetIdQuery(ctx *gin.Context) (uint, error) {
 	return uint(parsedId), nil
 }
 
-func GetPaginationData(ctx *gin.Context) (uint, uint, error) {
-	itemsPerPage := ctx.Query("itemsPerPage")
-	if itemsPerPage == "" {
-		return 0, 0, ErrParamIsRequired("id", "queryParameter")
-	}
-
-	parsedItemsPerPage, err := strconv.ParseUint(itemsPerPage, 10, 64)
-	if err != nil {
-		return 0, 0, fmt.Errorf("invalid parsed itemsPerPage: %v", err)
-	}
-
-	currentPage := ctx.Query("currentPage")
-	if currentPage == "" {
-		return 0, 0, ErrParamIsRequired("id", "queryParameter")
-	}
-
-	parsedCurrentPage, err := strconv.ParseUint(currentPage, 10, 64)
-	if err != nil {
-		return 0, 0, fmt.Errorf("invalid parsed currentPage: %v", err)
-	}
-
-	return uint(parsedItemsPerPage), uint(parsedCurrentPage), nil
-}
-
 func ReadBody[T any](ctx *gin.Context, input *T) error {
 	if err := ctx.ShouldBindJSON(&input); err != nil {
 		return fmt.Errorf("error to parsed body to json")
