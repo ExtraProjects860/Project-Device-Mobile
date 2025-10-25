0e6af478004d964851f62b3c6460a5c6e662ef72
DaviRodrigues <oversouls11@gmail.com>
2025-10-25 02:56:26 -0300

    feat: adding id primary key and softdeletes

--- backend/api/schemas/wishlist.go
+++ backend/api/schemas/wishlist.go
@@ -2,13 +2,16 @@ package schemas
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type WishList struct {
-	UserID    uint      `gorm:"primaryKey"`
-	ProductID uint      `gorm:"primaryKey"`
-	CreatedAt time.Time `gorm:"default:current_timestamp"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	gorm.Model
+	UserID    uint           `gorm:"primaryKey"`
+	ProductID uint           `gorm:"primaryKey"`
+	CreatedAt time.Time      `gorm:"default:current_timestamp"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
 
 	User    User
 	Product Product
