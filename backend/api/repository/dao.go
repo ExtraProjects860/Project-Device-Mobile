471ab50606bab32dac8d958efc8aff0d4a7791d9
DaviRodrigues <oversouls11@gmail.com>
2025-10-25 02:52:48 -0300

    feat: implemented filterSearch to used Memo, and modify method getByPagination

--- backend/api/repository/dao.go
+++ backend/api/repository/dao.go
@@ -2,9 +2,15 @@ package repository
 
 import (
 	"context"
+	"reflect"
+	"strings"
+	"sync"
 
+	"github.com/ExtraProjects860/Project-Device-Mobile/handler/request"
 	"gorm.io/gorm"
 )
+
+var searchableFieldsCache sync.Map
 
 func firstWhere[T any](db *gorm.DB, query string, args ...any) (T, error) {
 	var model T
@@ -48,13 +54,68 @@ func delete[T any](ctx context.Context, db *gorm.DB, entity *T, queryWhere strin
 	})
 }
 
-func getByPagination[T any](db *gorm.DB, itemsPerPage, currentPage uint) ([]T, uint, uint, error) {
+func filterSearch[T any](query *gorm.DB, search string) *gorm.DB {
+	if search == "" {
+		return query
+	}
+
+	var fields []string
+	typ := reflect.TypeOf((*T)(nil)).Elem()
+
+	if cached, ok := searchableFieldsCache.Load(typ); ok {
+		fields = cached.([]string)
+	}
+
+	var model T
+	stmt := &gorm.Statement{DB: query}
+	if err := stmt.Parse(&model); err != nil {
+		return query
+	}
+
+	for _, field := range stmt.Schema.Fields {
+		if field.DBName == "" ||
+			field.DBName == "created_at" ||
+			field.DBName == "updated_at" ||
+			field.DBName == "deleted_at" {
+			continue
+		}
+
+		if field.FieldType.Kind() != reflect.String {
+			continue
+		}
+
+		fields = append(fields, field.DBName)
+
+		searchableFieldsCache.Store(typ, fields)
+	}
+
+	var args []any
+	var whereParts []string
+	for _, col := range fields {
+		whereParts = append(whereParts, col+" LIKE ?")
+		args = append(args, "%"+search+"%")
+	}
+
+	return query.Where(
+		strings.Join(whereParts, " OR "),
+		args...,
+	)
+}
+
+func getByPagination[T any](db *gorm.DB, paginationSearch request.PaginationSearch) ([]T, uint, uint, error) {
 	var models []T
 
-	offset, totalPages, totalItems := pagination(db, itemsPerPage, currentPage)
+	offset, totalPages, totalItems := pagination(
+		db,
+		paginationSearch.ItemsPerPage,
+		paginationSearch.CurrentPage,
+	)
+
+	db = filterSearch[T](db, paginationSearch.SearchFilter)
 
 	err := db.
-		Limit(int(itemsPerPage)).
+		Order("id " + paginationSearch.ItemsOrder.String()).
+		Limit(int(paginationSearch.ItemsPerPage)).
 		Offset(int(offset)).
 		Find(&models).Error
 
