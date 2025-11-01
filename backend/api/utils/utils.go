a1c46b97dd747bea25e5bfad16b38a466bb15f3a
DaviRodrigues <oversouls11@gmail.com>
2025-10-31 21:33:54 -0300

    feat: adding global rand in application, and generateRandomCode

--- backend/api/utils/utils.go
+++ backend/api/utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/go-playground/validator/v10"
 )
 
@@ -8,4 +11,17 @@ var validate *validator.Validate
 
 func init() {
 	validate = validator.New()
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func GetValidate() *validator.Validate {
+	return validate
+}
+
+func GenerateRandomCode(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = byte(rand.Intn(10) + '0')
+	}
+	return string(b)
 }
