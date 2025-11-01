99db0943fd301f1b9fa3eb6ddbaf4cf9f168a44a
DaviRodrigues <oversouls11@gmail.com>
2025-10-31 21:27:40 -0300

    refactor: correcting temaplte name, and adding cors in app

--- backend/microservices/src/main.py
+++ backend/microservices/src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import email, health_check
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 base_path = "/api/v1"
 
 app.include_router(router=email.router, prefix=base_path)
