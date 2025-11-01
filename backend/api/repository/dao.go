58732f49fc6779c8897e2a7472cfcda4c3b3bcc5
DaviRodrigues <oversouls11@gmail.com>
2025-10-31 21:36:26 -0300

    refactor and fix: moving pagination functions to pagination file and fix search param in db, but in wishlist he not was working

--- backend/api/repository/dao.go
+++ backend/api/repository/dao.go
@@ -2,11 +2,8 @@ package repository
 
 import (
 	"context"
-	"reflect"
-	"strings"
 	"sync"
 
-	"github.com/ExtraProjects860/Project-Device-Mobile/handler/request"
 	"gorm.io/gorm"
 )
 
@@ -52,72 +49,4 @@ func delete[T any](ctx context.Context, db *gorm.DB, entity *T, queryWhere strin
 	return db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
 		return tx.Model(new(T)).Where(queryWhere, args...).Delete(entity).Error
 	})
-}
-
-func filterSearch[T any](query *gorm.DB, search string) *gorm.DB {
-	if search == "" {
-		return query
-	}
-
-	var fields []string
-	typ := reflect.TypeOf((*T)(nil)).Elem()
-
-	if cached, ok := searchableFieldsCache.Load(typ); ok {
-		fields = cached.([]string)
-	}
-
-	var model T
-	stmt := &gorm.Statement{DB: query}
-	if err := stmt.Parse(&model); err != nil {
-		return query
-	}
-
-	for _, field := range stmt.Schema.Fields {
-		if field.DBName == "" ||
-			field.DBName == "created_at" ||
-			field.DBName == "updated_at" ||
-			field.DBName == "deleted_at" {
-			continue
-		}
-
-		if field.FieldType.Kind() != reflect.String {
-			continue
-		}
-
-		fields = append(fields, field.DBName)
-
-		searchableFieldsCache.Store(typ, fields)
-	}
-
-	var args []any
-	var whereParts []string
-	for _, col := range fields {
-		whereParts = append(whereParts, col+" ILIKE ?")
-		args = append(args, "%"+search+"%")
-	}
-
-	return query.Where(
-		strings.Join(whereParts, " OR "),
-		args...,
-	)
-}
-
-func getByPagination[T any](db *gorm.DB, paginationSearch request.PaginationSearch) ([]T, uint, uint, error) {
-	var models []T
-
-	offset, totalPages, totalItems := pagination(
-		db,
-		paginationSearch.ItemsPerPage,
-		paginationSearch.CurrentPage,
-	)
-
-	db = filterSearch[T](db, paginationSearch.SearchFilter)
-
-	err := db.
-		Order("id " + paginationSearch.ItemsOrder.String()).
-		Limit(int(paginationSearch.ItemsPerPage)).
-		Offset(int(offset)).
-		Find(&models).Error
-
-	return models, totalPages, totalItems, err
 }
--- backend/api/repository/pagination.go
+++ backend/api/repository/pagination.go
@@ -2,9 +2,16 @@ package repository
 
 import (
 	"math"
+	"reflect"
+	"strings"
 
+	"github.com/ExtraProjects860/Project-Device-Mobile/handler/request"
 	"gorm.io/gorm"
 )
+
+type SearchableModel interface {
+	GetSearchableFields() []string
+}
 
 func count(query *gorm.DB) uint {
 	var count int64
@@ -39,4 +46,57 @@ func pagination(query *gorm.DB, itemsPerPage uint, currentPage uint) (uint, uint
 	paginationOffset := (currentPage - 1) * itemsPerPage
 
 	return paginationOffset, totalPages, lengthItems
+}
+
+func filterSearch[T SearchableModel](query *gorm.DB, search string) *gorm.DB {
+	if search == "" {
+		return query
+	}
+
+	var fields []string
+	typ := reflect.TypeOf((*T)(nil)).Elem()
+
+	if cached, ok := searchableFieldsCache.Load(typ); ok {
+		fields = cached.([]string)
+	} else {
+		var model T
+		fields = model.GetSearchableFields()
+		searchableFieldsCache.Store(typ, fields)
+	}
+
+	if len(fields) == 0 {
+		return query
+	}
+
+	var args []any
+	var whereParts []string
+	for _, col := range fields {
+		whereParts = append(whereParts, col+" ILIKE ?")
+		args = append(args, "%"+search+"%")
+	}
+
+	return query.Where(
+		strings.Join(whereParts, " OR "),
+		args...,
+	)
+}
+
+func getByPagination[T SearchableModel](db *gorm.DB, paginationSearch request.PaginationSearch) ([]T, uint, uint, error) {
+	var models []T
+
+	db = filterSearch[T](db, paginationSearch.SearchFilter)
+
+	offset, totalPages, totalItems := pagination(
+		db,
+		paginationSearch.ItemsPerPage,
+		paginationSearch.CurrentPage,
+	)
+
+	err := db.
+		Order("id " + paginationSearch.ItemsOrder.String()).
+		Limit(int(paginationSearch.ItemsPerPage)).
+		Offset(int(offset)).
+		Find(&models).Error
+
+	return models, totalPages, totalItems, err
 }
